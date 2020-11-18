c72625a465b43e010a641c579706693b74956dfc
HassnainNisar <hhn164393@gmail.com>
2020-11-18 21:37:09 +0500

    update in server file

--- server.js
+++ server.js
@@ -4,9 +4,12 @@
 
  const PORT = process.env.PORT || 3000
 
- app.listen(PORT ,()=> (
-     console.log("listening on PORT " +PORT)
-     )
- )
+ app.listen(PORT ,()=> {
+     console.log("listening on PORT " + PORT)
+ })
+
+ const expressLayout = require('express-ejs-layouts')
+
+     app.use(expressLayout)
 
 
