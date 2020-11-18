9b9fa372ae0da072c0b709f25150f80d1adcbc39
HassnainNisar <hhn164393@gmail.com>
2020-11-18 22:28:33 +0500

    updated project structures

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
- const express = require('express')
+const express = require('express')
 
- const app = express()
+const app=express()
 
- const PORT = process.env.PORT || 3000
+const ejs= require ('ejs')
+const expressLayout = require( 'express-ejs-layouts')
 
- app.listen(PORT ,()=> {
-     console.log("listening on PORT " + PORT)
- })
+const PORT= process.env.PORT || 3000
 
- const expressLayout = require('express-ejs-layouts')
+const path= require ('path')
 
-     app.use(expressLayout)
+app.get('/' , (req,res) =>{
+        res.render('home')
+    }
+)
 
 
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+
+app.set('view engine' , 'ejs')
+
+app.listen(3000, () => {
+    console.log(`listening on port ${PORT}` )
+})
